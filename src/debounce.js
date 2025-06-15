273d11ae2526ef06fb009e6ead3f827edda7a6fd
Isabela de Gondra <66333927+Gondrai@users.noreply.github.com>
2025-06-15 17:10:15 -0300

    Update and rename Novo Documento de Texto.txt to debounce.js

---
+++ src/debounce.js
@@ -0,0 +1,13 @@
+// src/debounce.js
+
+function debounce(func, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+
+module.exports = debounce;
