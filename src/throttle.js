db5e1053ce249b21434bdcedb802d184cb6eff3b
Isabela de Gondra <66333927+Gondrai@users.noreply.github.com>
2025-06-15 17:10:42 -0300

    Create throttle.js

---
+++ src/throttle.js
@@ -0,0 +1,14 @@
+// src/throttle.js
+
+function throttle(func, limit) {
+  let inThrottle;
+  return function (...args) {
+    if (!inThrottle) {
+      func.apply(this, args);
+      inThrottle = true;
+      setTimeout(() => inThrottle = false, limit);
+    }
+  };
+}
+
+module.exports = throttle;
