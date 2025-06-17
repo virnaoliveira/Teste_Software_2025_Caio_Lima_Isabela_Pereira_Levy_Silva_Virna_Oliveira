c07841ed3ff882c25c48c1c0890760eb9b92ba37
Caio Rosberg <78824611+CaioRxl@users.noreply.github.com>
2025-06-16 21:10:20 -0300

    Add files via upload

---
+++ StreamFromString.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Xunit;
+using MeuProjeto;
+
+namespace MeuProjeto.Tests
+{
+    public class CsvProcessorTests
+    {
+
+        [Fact]
+        public void ToStream_DeveConverterStringParaStreamComConteudoCorreto()
+        {
+            string textoOriginal = "Olá, mundo!";
+
+            using var streamResultado = textoOriginal.ToStream();
+
+            using var reader = new StreamReader(streamResultado);
+            string textoNoStream = reader.ReadToEnd();
+
+            Assert.Equal(textoOriginal, textoNoStream);
+        }
+
+    }
+}
